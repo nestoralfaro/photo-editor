ecac6c1253102a7c77d741b5a43edec71dcd785c
Nestor <nalfaro@harding.edu>
2022-09-20 23:15:18 -0700

    Changed Form1 to MainForm

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace photo_editor
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,17 @@ namespace photo_editor
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace photo_editor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
