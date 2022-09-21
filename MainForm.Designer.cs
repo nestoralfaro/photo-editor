65af83be489f09c8c723118804cee9a23c69272f
Nestor <nalfaro@harding.edu>
2022-09-20 23:42:19 -0700

    Changed .NET version from 4.8.1 to 4.6 as I was having problems running with the latest version.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,20 +29,69 @@ namespace photo_editor
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.fileMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.locateOnDiskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectRootFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // fileMenuStrip
+            // 
+            this.fileMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.locateOnDiskToolStripMenuItem,
+            this.selectRootFolderToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileMenuStrip.Name = "fileMenuStrip";
+            this.fileMenuStrip.Size = new System.Drawing.Size(179, 76);
+            this.fileMenuStrip.Text = "File";
+            // 
+            // locateOnDiskToolStripMenuItem
+            // 
+            this.locateOnDiskToolStripMenuItem.Name = "locateOnDiskToolStripMenuItem";
+            this.locateOnDiskToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.locateOnDiskToolStripMenuItem.Text = "Locate on Disk";
+            // 
+            // selectRootFolderToolStripMenuItem
+            // 
+            this.selectRootFolderToolStripMenuItem.Name = "selectRootFolderToolStripMenuItem";
+            this.selectRootFolderToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.selectRootFolderToolStripMenuItem.Text = "Select Root Folder...";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(175, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "R&D\'s Photo Editor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.fileMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip fileMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem locateOnDiskToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectRootFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -16,5 +16,10 @@ namespace photo_editor
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
